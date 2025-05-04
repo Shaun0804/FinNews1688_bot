24be29194a02471db1981f31dc0c8cd7cf5a7d70
Shaun0804 <132467981+Shaun0804@users.noreply.github.com>
2025-05-04 21:11:35 +0800

    Add files via upload

--- bot.py
+++ bot.py
@@ -72,4 +72,6 @@ def init_webhook():
 
 # === 啟動 Flask App ===
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
