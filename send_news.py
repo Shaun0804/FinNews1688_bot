10d79b202dedc26b6ca4289b41e11c57a2a7d688
Shaun0804 <132467981+Shaun0804@users.noreply.github.com>
2025-05-20 13:15:29 +0800

    Update send_news.py

--- send_news.py
+++ send_news.py
@@ -6,8 +6,8 @@ from datetime import datetime
 import os
 
 # 從 GitHub Actions Secrets 讀取
-TOKEN = os.getenv("7915485999:AAHSYzBi1-Hh8PRvRRhbmnuafsey8BdNS8o", "")
-CHAT_ID = os.getenv("8079438887", "")
+TOKEN = '7915485999:AAHSYzBi1-Hh8PRvRRhbmnuafsey8BdNS8o'
+CHAT_ID = '8079438887'
 
 bot = telegram.Bot(token=TOKEN)
 
